e6a4987f92cc8a4aafe88cfea454bd4f4a158d7d
thejsj <jorge.silva@thejsj.com>
2014-11-10 15:13:24 -0800

    Adding grunt and testing dependencies

---
+++ spec/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // CSS & Sass
+    sass: {
+      dist: {
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: {
+          'client/styles/style.css': 'client/styles/style.scss'
+        }
+      }
+    },
+    watch: {
+      // grunt: {
+      //   files: ['Gruntfile.js']
+      // },
+      sass: {
+        files: 'client/styles/**/*.scss',
+        tasks: ['sass']
+      },
+      options: {
+        livereload: true,
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Tasks
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['sass']);
+};
